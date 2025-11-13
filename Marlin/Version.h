bb173a3b1e697bf5216a50126efb5149444a228d
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-11-13 12:42:52 -0600

    🐛 Fix Teensy 4.x stepper timing (#28169)
    🧑‍💻 Timer general cleanup
    🩹 Teensy 4.x timer mods

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.9.8"
+//#define SHORT_BUILD_VERSION "2.0.9.9"
 
 /**
  * Verbose version identifier which should contain a reference to the location
